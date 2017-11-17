49436430ee16a245843b2483e69fe22409ccd162
zcdny <151210739@qq.com>
2017-11-17 00:56:41 -0800

    update dir path

--- index.php
+++ index.php
@@ -19,14 +19,16 @@ define("DIR_RES",		"module/theme/");
 define("DIR_UPLOAD",	"upload/");
 
 
-// default file
-require_once(PATH_MOD	."data/cfg.php");
+// default config
+$custom_file = PATH_MOD."custom.php";
+if (file_exists($custom_file)) {
+	require_once($custom_file); 
+} else {
+	require_once(PATH_MOD	."data/cfg.php");
+}
 require_once(PATH_MOD	."admin/common.php");
 require_once(PATH_MOD 	."data/access.php");
 
-// custom file
-$custom_file = PATH_MOD."custom.php";
-if (file_exists($custom_file)) { require_once($custom_file); }
 
 // index file
 require_once(PATH_MOD . $t['_m'].'/'.$t['_f'].'.php');
