a39a7729e7f50371fc775cb4f750735c2c45b928
linyu <151210739@qq.com>
2018-08-12 21:52:53 -0400

    v1.0.3.1

--- common/config.php
+++ common/config.php
@@ -5,7 +5,7 @@ $c = array();	// global config
 $t = array();	// template var
 
 
-$c['version'] 			=	'v1.0.3';
+$c['version'] 			=	'v1.0.3.1';
 $c['sql_server']		=	'localhost';
 $c['sql_user'] 			=	'cms_user';
 $c['sql_pawd'] 			=	'cms_pawd';
